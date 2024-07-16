0e2a3c4cd9eaf5186000d114988d12ffc8f0a742
narayan <narayanthakur4979@gmail.com>
2024-07-16 23:49:28 +0530

    merch link

--- RIC/RIC/settings.py
+++ RIC/RIC/settings.py
@@ -196,15 +196,6 @@ USE_TZ = True
 # EMAIL_HOST_PASSWORD = "onbzpxaipscnpvhg"
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp-mail.outlook.com"
-EMAIL_HOST_USER = "research_conclave@iitg.ac.in"
-EMAIL_HOST_PASSWORD = "S@B@23R!c"
-OUTLOOK_FOLDER = "Chetan_2023"
-
-
 
 
 
